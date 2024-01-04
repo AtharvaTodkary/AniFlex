ccc349f37d611d7a335591ea68783b45c2c49a63
Atharva Todkary <100767857+AtharvaTodkary@users.noreply.github.com>
2024-01-04 09:23:24 +0530

    Add files via upload

---
+++ Anime Website/login.js
@@ -0,0 +1,8 @@
+$("#email").click(function (){
+    $("#pemail").animate({"font-size":"12px",margin:"-11% 22%"});
+});
+$("#pswd").click(function (){
+    $("#ppswd").animate({"font-size":"12px",margin:"-16% 22%"});
+});
+
+    
